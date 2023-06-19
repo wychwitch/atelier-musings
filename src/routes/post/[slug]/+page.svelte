e26f363432da7bed3297f8d4bd5a9c0d996cdf64
wych(witch) <wych@wychwit.ch>
2023-06-18 23:46:26 -0500

    moved posts to /post 2

---
+++ src/routes/post/[slug]/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+import Tags from '$lib/components/Tags.svelte'
+export let data;
+const { title, date, Content, tags, edited} = data
+</script>
+<article >
+  <h1 class="text-center">{ title }</h1>
+  {#if data.tags.length}
+    <aside class="text-center">
+      <Tags tags={data.tags} />
+    </aside>
+  {/if}
+  <p class="text-right m-0">Published: {date}</p>
+  {#if data.edited}
+    <p class="text-right m-0">Edited: {edited}</p>
+  {/if}
+  <Content />
+</article>
+
+
+<style>
+  article{
+    padding: 1rem 3rem;
+  }
+  
+
+</style>
---
+++ src/routes/post/[slug]/+page.ts
@@ -0,0 +1,16 @@
+export async function load({ params }){
+  if(params.slug != "favicon.svg"){
+    const post = await import(`../../../posts/${params.slug}.md`)
+    const { title, date, tags, edited} = post.metadata
+    const Content = post.default
+
+
+    return {
+      Content,
+      title,
+      date,
+      tags,
+      edited
+    }
+  }
+}
